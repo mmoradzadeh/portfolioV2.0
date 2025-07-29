0d561baa68fc1e7ea687e8ff57df9f38064857e6
Mehdi Moradzadeh <morad97mm@gmail.com>
2025-07-29 00:31:59 -0400

    Changed the useRef to useState method to prepare for React Form Hook library to streamline the form submission

--- client/src/components/ContactForm/ContactFrom.tsx
+++ client/src/components/ContactForm/ContactFrom.tsx
@@ -1,24 +1,16 @@
-import React, { useRef } from "react"
+import React, { useState } from "react"
 import type { FormEvent } from "react";
 
 const ContactFrom: React.FC = () => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const emailRef = useRef<HTMLInputElement>(null);
-    const messageRef = useRef<HTMLTextAreaElement>(null);
-    const person = {
+    const [person, setPerson] = useState({
         name: '',
         email: '',
         message: ''
-    };
-
+    })
     const handleFormSubmit = (event: FormEvent) => {
         event.preventDefault();
-        if (nameRef.current !== null && emailRef.current !== null && messageRef.current !== null) {
-            person.name = nameRef.current.value;
-            person.email = emailRef.current.value;
-            person.message = messageRef.current.value;
-            console.log(person);
-        }
+        console.log(person);
+
     };
     return (
         <div className="col-lg-7">
@@ -27,15 +19,15 @@ const ContactFrom: React.FC = () => {
                 <form onSubmit={handleFormSubmit}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input ref={nameRef} type="text" className="form-control" id="name" required />
+                        <input onChange={(event) => setPerson({ ...person, name: event.target.value })} type="text" className="form-control" id="name" required />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input ref={emailRef} type="email" className="form-control" id="email" required />
+                        <input onChange={(event) => setPerson({ ...person, email: event.target.value })} type="email" className="form-control" id="email" required />
                     </div>
                     <div className="mb-4">
                         <label htmlFor="message" className="form-label">Message</label>
-                        <textarea ref={messageRef} className="form-control" id="message" rows={4} required></textarea>
+                        <textarea onChange={(event) => setPerson({ ...person, message: event.target.value })} className="form-control" id="message" rows={4} required></textarea>
                     </div>
                     <button type="submit" className="btn btn-dark w-100 py-3">
                         Send Message
